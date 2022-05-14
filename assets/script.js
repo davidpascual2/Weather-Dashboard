b11ebb9ecb8402f32642a4428598b6e6699fe559
dave <davidpascual200@gmail.com>
2022-05-14 16:35:03 -0700

    completed fetch request

--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,9 @@ function currentWeather(event) {
     event.preventDefault() //without this error uncaught can occur
     let searchCity = document.querySelector("#search-city").value; //value extracts value from html input
         console.log(searchCity)
+        
     let latLonAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=1&appid=${APIKey}`;
-    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`;
-    
+
     fetch(latLonAPI)
     .then(function(response){
         console.log(response)
@@ -29,43 +29,34 @@ function currentWeather(event) {
         // else {
         //     // alert("error") //fix
         // }
-    }).then(function(response){
+    })
+    .then(function(response){
         console.log(response)
 
-        let lat = response[0].lat // "store" response in variable
+        let lat = response[0].lat // "stores" response in variable
         let lon = response[0].lon
         let cityName = response[0].name
-
         console.log(lat, lon)
-    })
-    .catch(function(error){
-        // alert("unable to connect")
-        console.log(error)
-    });
 
-    
-    fetch(apiURL)
+        
+        let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`;
+
+        fetch(apiURL)
         .then(function(response){
             console.log(response)
             if(response.ok) {
                 return response.json()
             }
         })
-    
-//     .then(function(response){ //.then(response) => { ........... // .then(res => res.json())
-//         console.log(response);
+        .then(function (response) {
+            console.log(response) //display function????
+        })
+    })
 
-//         if (response.ok) {
-//             response.json().then(function(data) {
-//                 currentWeather(data)
-//             })
-//         } else {
-//             // alert("error") //fix
-//         }
-//     })
-//     .catch(function(error){
-//         // alert("unable to connect")
-// });
+    .catch(function(error){
+        // alert("unable to connect")
+        console.log(error)
+    });
 }
 
 
@@ -84,3 +75,40 @@ function renderCities() {
     //create array  and loop through
     //use cards
     //append 5 day forcast
+    
+    function callWeatherApi() {
+        // Get lat + long coordinates first 
+        let userSearch = document.querySelector("#location-search").value;
+        let coordinateUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + userSearch + "&limit=1&appid=" + APIkey;
+      
+        fetch(coordinateUrl)
+          .then(function (response) {
+            // Add modal to alert user if city name cannot be found 
+            if (!response.ok) {
+              return;
+            }
+            return response.json();
+          })
+          .then(function (data) {
+            console.log(data);
+            let lat = data[0].lat
+            let long = data[0].lon
+            let cityName = data[0].name
+            displayCityName(cityName);
+            addSearchHistory(cityName);
+      
+            let oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=hourly,minutely&appid=" + APIkey;
+      
+            fetch(oneCallUrl)
+              .then(function (response) {
+                return response.json()
+              })
+              .then(function (data) {
+                // display results on page SOMEHOW
+                printResults(data);
+      
+              })
+          }
+          );
+      
+      };
