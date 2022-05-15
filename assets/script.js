b1a83584addef743f54bf64f295b452877036831
dave <davidpascual200@gmail.com>
2022-05-14 20:31:49 -0700

    fixed fetch request

--- assets/script.js
+++ assets/script.js
@@ -2,55 +2,58 @@
 var APIKey = '8635650944450d2222bebb2cadec5082';
 var userFormEl = document.querySelector("#user-form")
 
-var inputEl = document.querySelector("#search-city");
+var inputEl = document.querySelector("#search-input"); //coment out? id already defined elsewhere
 var buttonEl = document.querySelector("#search-button");
 var searchHistoryEl = document.querySelector(".search-history");
 var currentWeatherEl = document.querySelector(".current-weather");
-var ForecastEl = document.querySelector(".forecast");
+var searchedCityEl = document.querySelector("#searched-city");
+var fiveDayForecastEl = document.querySelector(".five-day-forecast");
 // var cityNameEl = document.querySelector(".city-name")
 
 //create event listener for search button
-buttonEl.addEventListener("click", currentWeather);
+buttonEl.addEventListener("click", callWeatherAPI);
 
 //use fetch request to get current condtions on open weather maps (url fed to fetch)
-function currentWeather(event) {
+function callWeatherAPI(event) {
     event.preventDefault() //without this error uncaught can occur
-    let searchCity = document.querySelector("#search-city").value; //value extracts value from html input
+    let searchCity = document.querySelector("#search-input").value; //value extracts value from html input
         console.log(searchCity)
         
     let latLonAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=1&appid=${APIKey}`;
 
     fetch(latLonAPI)
-    .then(function(response){
-        console.log(response)
+        .then(function(response){
+        // console.log(response)
         if(response.ok) {
             return response.json()
         } 
         // else {
         //     // alert("error") //fix
         // }
-    })
-    .then(function(response){
+        })
+        .then(function (response) {
         console.log(response)
 
         let lat = response[0].lat // "stores" response in variable
         let lon = response[0].lon
         let cityName = response[0].name
         console.log(lat, lon)
-
+        displayWeather(cityName);
+        
         
         let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`;
 
         fetch(apiURL)
-        .then(function(response){
-            console.log(response)
-            if(response.ok) {
+            .then(function(response){
+                // console.log(response)
                 return response.json()
-            }
-        })
-        .then(function (response) {
-            console.log(response) //display function????
-        })
+            })
+        
+            .then(function (response) {
+                console.log(response) //display function????
+                displayWeather(response)
+            })
+        
     })
 
     .catch(function(error){
@@ -59,9 +62,20 @@ function currentWeather(event) {
     });
 }
 
+let cityName =document.createElement("h2")
 
+function displayWeather(cityName) {
+//     currentWeatherEl.innerHTML = "";
+//     cityName.classList.add("#searched-city");
+    
+
+
+    
+    
+
+// }
 //create function to display 5 day forcast 
-function weatherForcast(){
+function fiveDayForcast() {
 
 }
 
@@ -69,10 +83,23 @@ function weatherForcast(){
 
 
 //create function to render recently searched cities
-function renderCities() {
+function recentSearches() {
     
 }
     //create array  and loop through
     //use cards
     //append 5 day forcast
     
+
+
+
+
+    // <p class="card-text">High Feels like ${
+    //     day.feels_like.day
+    //   }&deg;C</p>
+    //   <p class="card-text">Pressure ${day.pressure}mb</p>
+    //   <p class="card-text">Humidity ${day.humidity}%</p>
+    //   <p class="card-text">UV Index ${day.uvi}</p>
+    //   <p class="card-text">Precipitation ${day.pop * 100}%</p>
+    //   <p class="card-text">Dewpoint ${day.dew_point}</p>
+    //   <p class="card-text">Wind ${day.wind_speed}m/s, ${
